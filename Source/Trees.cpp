0c1cdce761dbc8c99bce6c6f377b96b2a46b8508
Wkemery <wemery1@cslab15.cs.uwyo.edu>
2017-12-04 00:34:53 -0700

    wrote tagtree:readIn(), lots of TODO's but i think the read and write for tag tree are mostly done. at least testable. Need to do root read in and write out. Gonaa have an constant blocknum for root, but that will be decided upon first run. when partitions are set and the number of blocks are picked as well as the blocksize. that info will be acceisable through the master superblock which contains partition information, and therefore we'll be able to determine where the root tree block starts on any partition.

--- Source/Trees.cpp
+++ Source/Trees.cpp
@@ -57,6 +57,8 @@ BlkNumType TagTree::getBlockNum() {return _blockNumber;}
 
 void TagTree::writeOut(PartitionManager* pm)
 {
+  //TODO: zero out the first byte of the key on disk for the rest of the block 
+  //TODO: and any continuation blocks to account for any deletions when it was in memory 
   char* buff = new char[pm->getBlockSize()];
   memset(buff, 0, pm->getBlockSize()); //zero out memory
   int offset = 0;
@@ -79,7 +81,7 @@ void TagTree::writeOut(PartitionManager* pm)
       //TODO: fix catch statement.
       BlkNumType newblknum = 0;
       try{newblknum = pm->getFreeDiskBlock();}
-      catch(...) {cerr << "Error trees.cpp2" << endl;}
+      catch(...) {cerr << "Error trees.cpp3" << endl;}
       
       /*got a new block, set it as continuation.*/
       memcpy(buff + (pm->getBlockSize() - sizeof(BlkNumType)), &newblknum, sizeof(BlkNumType));
@@ -108,17 +110,74 @@ void TagTree::writeOut(PartitionManager* pm)
     
     /*write out the blocknumber of the fidentifier to buffer, probably 8 bytes?*/
     BlkNumType fiden = it->second->getFidentifier();
-    memcpy(buff + (offset*sizeof(char)), &fiden, sizeof(BlkNumType));    
+    memcpy(buff + (offset*sizeof(char)), &fiden, sizeof(BlkNumType));
+    offset += sizeof(BlkNumType);
   }
 }
 
 void TagTree::readIn(PartitionManager* pm)
 {
-  //TODO:stub
-  //TODO: implement block continuation
+  char* buff = new char[pm->getBlockSize()];
+  memset(buff, 0, pm->getBlockSize()); //zero out memory
+  int offset = 0;
+  BlkNumType currentBlkNum = _blockNumber;
+  
+  /*Read in first block*/
+  //TODO: fix catch statement.
+  try{pm->readDiskBlock(currentBlkNum, buff);}
+  catch(...){cerr << "Error trees.cpp5" << endl;}
   
   
+  bool reading = buff[0] == 0;
   
+  /*For every entry on disk*/  
+  while(reading)//assuming that offset always points to the start of an entry name.
+  {
+//     char * temp = new char[65]; //65 to accomodate the potential missing null byte
+//     memset(temp, 0, 65);
+    
+//     strncpy(temp, buff + offset, 64);
+    string fileName;
+    //TODO: this may or may not work... uncomment neaby lines if needed
+//     fileName.assign(temp);
+    fileName.assign(buff + offset, 64);
+//     delete temp;
+    offset+= 64;
+    
+    /*Read in the blknum*/
+    BlkNumType blknum;
+    memcpy(&blknum, buff + offset, sizeof(BlkNumType));
+    
+    FileInfo* finode = new FileInfo(fileName, blknum);
+    
+    /*Insert key an value into tagtree in memory*/
+    _tree.insert(pair<string, FileInfo*>(fileName, finode));
+    //TODO: check insert ret val, if failed throw error someone fucked up
+    offset += sizeof(BlkNumType);
+    
+    /**************************************************************************/ 
+    /*check to see if there is another entry in this block and if there is a cont. block*/
+    
+    int entrySize = 64 + (sizeof(BlkNumType)*2);
+    if((pm->getBlockSize() - offset) < entrySize)
+    {
+      /*can't be another entry in this block, check for cont. block*/
+      memcpy(&currentBlkNum, buff + (pm->getBlockSize() - sizeof(BlkNumType)), sizeof(BlkNumType));
+      
+      if(currentBlkNum == 0){reading = false;} //no cont. block. all done reading in tree
+      else
+      {
+        //Cont. block exists. read it in and start over.
+        /*Read in cont. block*/
+        //TODO: fix catch statement.
+        try{pm->readDiskBlock(currentBlkNum, buff);}
+        catch(...){cerr << "Error trees.cpp6" << endl;}
+        offset = 0;
+      }
+    }
+    
+    if(buff[offset] == 0){reading = false;} //not another entry in this block
+  }
 }
 
 void TagTree::zeroDisk()
