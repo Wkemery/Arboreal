5d7eb1772d1ace91f0649bc5dd7feae480551d35
Wyatt Emery <wyatt.k.emery@gmail.com>
2018-03-07 12:06:39 -0700

    completed documentation fo Disk.

--- Source/Filesystem/Backend/disk.cpp
+++ Source/Filesystem/Backend/disk.cpp
@@ -8,28 +8,28 @@ using std::ofstream;
 using std::ifstream;
 using std::ios;
 
-Disk::Disk(BlkNumType numblocks, size_t blksz, char *fname)
+Disk::Disk(BlkNumType numblocks, size_t blockSize, char *location)
 {
-  blkCount = numblocks;
-  diskSize = numblocks * blksz;
-  blkSize = blksz;
-  diskFilename = strdup(fname);
+  _blockCount = numblocks;
+  _diskSize = numblocks * blockSize;
+  _blockSize = blockSize;
+  _diskLocation = strdup(location);
 }
 
 Disk::~Disk()
 {
-  free(diskFilename);
+  free(_diskLocation);
 }
 
 void Disk::readDiskBlock(BlkNumType blknum, char *blkdata)
 {
-  if ((blknum < 0) || (blknum >= blkCount)) throw invalid_arg("Blocknumber is out bounds", "Disk::readDiskBlock");
-  ifstream f(diskFilename, ios::binary | ios::in);
+  if ((blknum < 0) || (blknum >= _blockCount)) throw invalid_arg("Blocknumber is out bounds", "Disk::readDiskBlock");
+  ifstream f(_diskLocation, ios::binary | ios::in);
   if (!f) throw disk_error("Unable to open Disk", "Disk::readDiskBlock");
-  f.seekg(blknum * blkSize);
+  f.seekg(blknum * _blockSize);
   if(f.bad()) throw disk_error("Disk Seek Error", "Disk::readDiskBlock");
   
-  f.read(blkdata, blkSize);
+  f.read(blkdata, _blockSize);
   if(f.bad()) throw disk_error("Disk Read Error", "Disk::readDiskBlock");
   
   f.close();
@@ -37,12 +37,16 @@ void Disk::readDiskBlock(BlkNumType blknum, char *blkdata)
 
 void Disk::writeDiskBlock(BlkNumType blknum, char *blkdata)
 {
-  if ((blknum < 0) || (blknum >= blkCount)) throw invalid_arg("Blocknumber is out bounds", "Disk::writeDiskBlock");
-  ofstream f(diskFilename, ios::binary | ios::in | ios::out );
+  if ((blknum < 0) || (blknum >= _blockCount)) throw invalid_arg("Blocknumber is out bounds", "Disk::writeDiskBlock");
+  ofstream f(_diskLocation, ios::binary | ios::in | ios::out );
   if (!f) throw disk_error("Unable to open Disk", "Disk::writeDiskBlock");
-  f.seekp(blknum * blkSize);
+  f.seekp(blknum * _blockSize);
   if(f.bad()) throw disk_error("Disk Seek Error", "Disk::writeDiskBlock");
-  f.write(blkdata, blkSize);
+  f.write(blkdata, _blockSize);
   if(f.bad()) throw disk_error("Disk Write Error", "Disk::writeDiskBlock");
   f.close();
 }
+
+
+size_t Disk::getBlockSize() {return (_blockSize);}
+int Disk::getBlockCount() {return (_blockCount);}
--- Source/Filesystem/Backend/disk.h
+++ Source/Filesystem/Backend/disk.h
@@ -7,19 +7,44 @@
 
 
 class Disk {
-  public:
+private:
 
-  BlkNumType diskSize;
-  size_t blkSize;
-  BlkNumType blkCount;
-  char *diskFilename;
-
-  Disk(BlkNumType numblocks, size_t blksz, char *fname);
-    ~Disk();
-    void readDiskBlock(BlkNumType blknum, char *blkdata);
-    void writeDiskBlock(BlkNumType blknum, char *blkdata);
-    size_t getBlockSize() {return (blkSize);};
-    int getBlockCount() {return (blkCount);};
+  BlkNumType _diskSize;
+  size_t _blockSize;
+  BlkNumType _blockCount;
+  char *_diskLocation;
+public:
+  /*!
+   * @param numblocks the number of blocks on the Disk
+   * @param blocksize the block size for Disk blocks
+   * @param location the location of the Disk
+   */
+  Disk(BlkNumType numblocks, size_t blockSize, char *location);
+  ~Disk();
+  
+  /*!
+   * Reads a block from the Disk. 
+  * @param blknum the blocknumber to be read
+  * @param blkdata the buffer to put the read data. must be large enough to contain an entire block of data
+  */
+  void readDiskBlock(BlkNumType blknum, char *blkdata);
+  
+  /*!
+   * Writes a block to the Disk. 
+   * @param blknum the blocknumber to be written
+   * @param blkdata the buffer to write the data from. It Will write an entire block size of data.
+   */
+  void writeDiskBlock(BlkNumType blknum, char *blkdata);
+  
+  /*!
+   * @returns the blocksize of the Disk
+  */
+  size_t getBlockSize();
+  
+  /*!
+   * @returns the number of blocks on the entire Disk
+  */
+  int getBlockCount();
 };
 
 #endif
